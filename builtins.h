64b790a4f6d68d635aad8e07010dc5249638deac
Sanjana Kaushik <skaushik@luc.edu>
2025-12-03 16:12:14 -0600

    Update builtins.h

--- builtins.h
+++ builtins.h
@@ -1,13 +1,19 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
+#include <stddef.h>
+
+struct builtin {
+  const char *name;
+  int (*handler)(char **);
+
 // Function declarations
-int lsh_cd(char **args);
-int lsh_echo(char **args);
-int lsh_pwd(char **args);
-int lsh_env(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
+int cmd_cd(char **args);
+int cmd_echo(char **args);
+int cmd_pwd(char **args);
+int cmd_env(char **args);
+int cmd_help(char **args);
+int cmd_exit(char **args);
 
 // Expose builtin command table
 extern char *builtin_str[];
